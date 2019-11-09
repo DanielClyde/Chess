266548fbba07b23230f493af1ce1b953460ec569
Jaxon Willard <jaxon.willard@aggiemail.usu.edu>
2019-11-08 20:16:23 -0700

    fixed getLegalMoves() in Pawn

--- src/Pawn.java
+++ src/Pawn.java
@@ -18,18 +18,38 @@ public class Pawn extends Piece {
         ArrayList<Position> moves = new ArrayList<>();
         int forwardTwo = this.isWhite ? 2 : -2;
         int forwardOne = this.isWhite ? 1 : -1;
-        if (this.pos.row+forwardOne >= 0 && !this.board.tiles[this.pos.col][this.pos.row+forwardOne].hasPiece){
-            moves.add(new Position(this.pos.col, this.pos.row+forwardOne));
-            if (this.pos.col + 1 <= 7 && this.board.tiles[this.pos.col+1][this.pos.row+forwardOne].hasPiece){
-                moves.add(new Position(this.pos.col+1, this.pos.row+forwardOne));
+        if (this.pos.row+forwardOne >= 0 && this.pos.row+forwardOne <= 7){
+            if (!this.board.tiles[this.pos.col][this.pos.row+forwardOne].hasPiece) {
+                moves.add(new Position(this.pos.col, this.pos.row+forwardOne));
+                if (this.pos.row+forwardTwo >= 0 && this.pos.row+forwardTwo <= 7){
+                    if (this.board.tiles[this.pos.col][this.pos.row+forwardTwo].hasPiece){
+                        if (this.board.tiles[this.pos.col][this.pos.row+forwardTwo].piece.isWhite ^ this.isWhite){
+                            moves.add(new Position(this.pos.col, this.pos.row+forwardTwo));
+                        }
+                    }else moves.add(new Position(this.pos.col, this.pos.row+forwardTwo));
+                }
+            }else {
+                if (this.board.tiles[this.pos.col][this.pos.row+forwardOne].piece.isWhite ^ this.isWhite){
+                    moves.add(new Position(this.pos.col, this.pos.row+forwardOne));
+                }
             }
-            if (this.pos.col - 1 >= 0 && this.board.tiles[this.pos.col-1][this.pos.row+forwardOne].hasPiece){
+}
+
+        if (this.pos.row+forwardOne >= 0 && this.pos.row+forwardOne <= 7 && this.pos.col-1 >= 0){
+            if (this.board.tiles[this.pos.col-1][this.pos.row+forwardOne].hasPiece && (
+                    this.board.tiles[this.pos.col-1][this.pos.row+forwardOne].piece.isWhite ^ this.isWhite)){
                 moves.add(new Position(this.pos.col-1, this.pos.row+forwardOne));
             }
-            if (this.isFirstMove && !this.board.tiles[this.pos.col][this.pos.row+forwardTwo].hasPiece){
-                moves.add(new Position(this.pos.col, this.pos.row+forwardTwo));
+        }
+
+        if (this.pos.row+forwardOne >= 0 && this.pos.row+forwardOne <= 7 && this.pos.col+1 >= 0){
+            if (this.board.tiles[this.pos.col+1][this.pos.row+forwardOne].hasPiece && (
+                    this.board.tiles[this.pos.col+1][this.pos.row+forwardOne].piece.isWhite ^ this.isWhite)){
+                moves.add(new Position(this.pos.col+1, this.pos.row+forwardOne));
             }
         }
+
+
 
 
 
