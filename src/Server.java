ceb6176e7af62b77f5d8ed85b5dd5a9d3e8efbc0
Danny Clyde <Dantheman27c@gmail.com>
2019-11-18 22:57:10 -0700

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -7,15 +7,15 @@ import java.util.concurrent.Executors;
 
 public class Server {
     private static Hashtable<Boolean, ObjectOutputStream> writers = new Hashtable<>();
-    private static boolean bothConnected;
 
     public static void main(String[] args) throws Exception {
         System.out.println("Chess Server is Running...");
         ExecutorService pool = Executors.newFixedThreadPool(2);
         try (ServerSocket listener = new ServerSocket(58901)) {
-            pool.execute(new Handler(listener.accept(), true));
-            pool.execute(new Handler(listener.accept(), false));
-            bothConnected = true;
+            while(true) {
+                pool.execute(new Handler(listener.accept(), true));
+                pool.execute(new Handler(listener.accept(), false));
+            }
         }
     }
 
@@ -38,10 +38,6 @@ public class Server {
                 this.input = new ObjectInputStream(socket.getInputStream());
                 this.output = new ObjectOutputStream(socket.getOutputStream());
                 writers.put(this.isWhite, output);
-                while(!bothConnected) {
-                    System.out.print("#");
-                }
-                System.out.println();
                 while (true) {
                     GameMessage m  = (GameMessage) this.input.readObject();
                     writers.get(!this.isWhite).writeObject(m);
