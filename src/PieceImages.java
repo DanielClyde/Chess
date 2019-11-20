fb8649605f6ea20aacc814dd732b257853e2cb33
Danny Clyde <Dantheman27c@gmail.com>
2019-11-20 12:51:54 -0700

    added new pictures

--- src/PieceImages.java
+++ src/PieceImages.java
@@ -5,15 +5,15 @@ import java.io.File;
 public class PieceImages {
     Image whiteBishop = new Image("/white_bishop.png");
     Image blackBishop = new Image("/black_bishop.png");
-    Image whiteKing   = new Image("/white_bishop.png");
-    Image blackKing   = new Image("/black_bishop.png");
-    Image whiteKnight = new Image("/white_bishop.png");
-    Image blackKnight = new Image("/black_bishop.png");
-    Image whitePawn   = new Image("/white_bishop.png");
-    Image blackPawn   = new Image("/black_bishop.png");
-    Image whiteQueen  = new Image("/white_bishop.png");
-    Image blackQueen  = new Image("/black_bishop.png");
-    Image whiteRook   = new Image("/white_bishop.png");
-    Image blackRook   = new Image("/black_bishop.png");
+    Image whiteKing   = new Image("/king_white.png");
+    Image blackKing   = new Image("/king_black.png");
+    Image whiteKnight = new Image("/knight_white.png");
+    Image blackKnight = new Image("/knight_black.png");
+    Image whitePawn   = new Image("/white_pawn.png");
+    Image blackPawn   = new Image("/pawn_black.png");
+    Image whiteQueen  = new Image("/queen_white.png");
+    Image blackQueen  = new Image("/queen_black.png");
+    Image whiteRook   = new Image("/rook_white.png");
+    Image blackRook   = new Image("/rook_black.png");
 
 }
