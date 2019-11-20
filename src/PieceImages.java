af3a2b8794c9ea1c870b69115c0ad921d4210a50
Danny Clyde <Dantheman27c@gmail.com>
2019-11-20 09:44:13 -0700

    Update PieceImages.java

--- src/PieceImages.java
+++ src/PieceImages.java
@@ -1,17 +1,19 @@
 import javafx.scene.image.Image;
 
+import java.io.File;
+
 public class PieceImages {
-    Image whiteBishop = new Image("https://www.pinclipart.com/picdir/big/60-602746_chess-piece-bishop-king-chessboard-alfil-ajedrez-para.png");
-    Image blackBishop = new Image("https://www.pinclipart.com/picdir/big/100-1006999_clipart-silhouette-chess-piece-remix-bishop-alfil-chess.png");
-    Image whiteKing   = new Image("https://www.pinclipart.com/picdir/big/69-695089_all-photo-png-clipart-king-chess-piece-clip.png");
-    Image blackKing   = new Image("https://www.pinclipart.com/picdir/big/82-825511_big-image-png-king-chess-piece-silhouette-clipart.png");
-    Image whiteKnight = new Image("https://www.pinclipart.com/picdir/big/191-1916865_horse-chess-piece-knight-comments-knight-chess-piece.png");
-    Image blackKnight = new Image("https://www.pinclipart.com/picdir/big/60-602828_chess-piece-knight-bishop-king-knight-chess-clip.png");
-    Image whitePawn   = new Image("https://www.pinclipart.com/picdir/big/203-2039435_bishop-rubber-stamp-chess-pawn-png-clipart.png");
-    Image blackPawn   = new Image("https://www.pinclipart.com/picdir/big/184-1845764_chess-piece-remix-pawn-pen-clipart.png");
-    Image whiteQueen  = new Image("https://www.pinclipart.com/picdir/big/65-658889_king-rubber-stamp-white-queen-chess-piece-png.png");
-    Image blackQueen  = new Image("https://www.pinclipart.com/picdir/big/352-3529225_king-chess-piece-shape-comments-queen-chess-piece.png");
-    Image whiteRook   = new Image("https://www.pinclipart.com/picdir/big/23-237242_rook-clip-art-clipart-chess-rook-clip-art.png");
-    Image blackRook   = new Image("https://www.pinclipart.com/picdir/big/67-676127_tiles-clipart-transparent-rook-chess-piece-clipart-png.png");
+    Image whiteBishop = new Image("/white_bishop.png");
+    Image blackBishop = new Image("/black_bishop.png");
+    Image whiteKing   = new Image("/white_bishop.png");
+    Image blackKing   = new Image("/black_bishop.png");
+    Image whiteKnight = new Image("/white_bishop.png");
+    Image blackKnight = new Image("/black_bishop.png");
+    Image whitePawn   = new Image("/white_bishop.png");
+    Image blackPawn   = new Image("/black_bishop.png");
+    Image whiteQueen  = new Image("/white_bishop.png");
+    Image blackQueen  = new Image("/black_bishop.png");
+    Image whiteRook   = new Image("/white_bishop.png");
+    Image blackRook   = new Image("/black_bishop.png");
 
 }
